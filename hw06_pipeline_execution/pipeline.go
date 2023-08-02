0bbb280c123e0d48cbe1a32789878279b6eff14e
EfremovAV <aleksandr.efremov@vitech.team>
2023-08-02 21:58:31 +0300

    HW06 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,31 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages { // Запускаем каждый stage, передаём на вход предыдущий результат
+		in = execStage(in, done, stage)
+	}
+
+	return in
+}
+
+func execStage(in In, done In, stageFunc Stage) Out {
+	ch := make(Bi)
+	go func() {
+		defer close(ch)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				// Собираем значения из входящего канала в новый канал,
+				// чтобы контролировать stage самостоятельно
+				ch <- v
+			}
+		}
+	}()
+
+	return stageFunc(ch)
 }
