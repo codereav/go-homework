966c9e8814aa65b1a6430a556bb95f444dc8f5e4
codereav <alex_uralsk14@mail.ru>
2023-09-18 21:50:54 +0300

    HW10 is completed goos: linux goarch: amd64 pkg: github.com/codereav/homework/hw10_program_optimization cpu: Intel(R) Core(TM) i5-7400 CPU @ 3.00GHz                 │     old      │              new               │                 │    sec/op    │    sec/op     vs base          │ GetDomainStat-4   7.363m ± 29%   7.462m ± 32%  ~ (p=0.631 n=10)
                    │      old       │                  new                  │
                    │      B/op      │     B/op       vs base                │
    GetDomainStat-4   2220.7Ki ± 31%   206.0Ki ± 17%  -90.72% (p=0.000 n=10)
    
                    │      old      │                 new                  │
                    │   allocs/op   │  allocs/op    vs base                │
    GetDomainStat-4   22.093k ± 31%   7.826k ± 17%  -64.58% (p=0.000 n=10)

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,11 +1,11 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
-	"regexp"
 	"strings"
+
+	jsoniter "github.com/json-iterator/go"
 )
 
 type User struct {
@@ -31,35 +31,34 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 type users [100_000]User
 
 func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
+	decoder := jsoniter.ConfigFastest.NewDecoder(r)
+	i := 0
+	for decoder.More() {
 		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
+		if err := decoder.Decode(&user); err != nil {
+			return result, err
 		}
 		result[i] = user
+		i++
 	}
-	return
+	return result, err
 }
 
 func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 
 	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
+		if user.Email == "" {
+			continue
 		}
+		userHost := strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])
+		if userHost == "" {
+			continue
+		}
+		userDomain := strings.ToLower(strings.SplitN(userHost, ".", 2)[1])
 
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		if userDomain == strings.ToLower(domain) {
+			result[userHost]++
 		}
 	}
 	return result, nil
--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -1,3 +1,4 @@
+//go:build !bench
 // +build !bench
 
 package hw10programoptimization
@@ -35,5 +36,11 @@ func TestGetDomainStat(t *testing.T) {
 		result, err := GetDomainStat(bytes.NewBufferString(data), "unknown")
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("find 'CAPS'", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "GOV")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{"browsedrive.gov": 1}, result)
 	})
 }
