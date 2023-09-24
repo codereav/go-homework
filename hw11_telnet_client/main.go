5c3d99f664b8cdfc2a810c4856ce8cecc267e005
codereav <alex_uralsk14@mail.ru>
2023-09-24 14:13:25 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,73 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var timeout time.Duration
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "Таймаут подключения")
+	flag.Parse()
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	client := NewTelnetClient(net.JoinHostPort(host, port), timeout, os.Stdin, os.Stdout)
+	defer func() {
+		if err := client.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	if err := client.Connect(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	go receive(ctx, stop, client)
+	go send(ctx, stop, client)
+
+	<-ctx.Done()
+}
+
+func send(ctx context.Context, stop context.CancelFunc, c TelnetClient) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if err := c.Send(); err != nil {
+				if errors.Is(err, io.EOF) {
+					fmt.Fprintln(os.Stderr, "...Connection was closed by peer")
+					stop()
+				}
+			}
+		}
+	}
+}
+
+func receive(ctx context.Context, stop func(), c TelnetClient) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if err := c.Receive(); err != nil {
+				os.Stderr.Write([]byte(err.Error()))
+				if errors.Is(err, io.EOF) {
+					fmt.Fprintln(os.Stderr, "...Connection was closed by peer")
+					stop()
+				}
+			}
+		}
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
+
+const bufferSize = 4096
 
 type TelnetClient interface {
 	Connect() error
@@ -13,9 +16,61 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &TnClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type TnClient struct {
+	in      io.ReadCloser
+	out     io.Writer
+	address string
+	timeout time.Duration
+	conn    net.Conn
+}
+
+func (c *TnClient) Connect() error {
+	var err error
+	c.conn, err = net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *TnClient) Send() error {
+	buffer := make([]byte, bufferSize)
+	i, err := c.in.Read(buffer)
+	if err != nil {
+		return err
+	}
+	_, err = c.conn.Write(buffer[:i])
+
+	return err
+}
+
+func (c *TnClient) Receive() error {
+	buffer := make([]byte, bufferSize)
+	i, err := c.conn.Read(buffer)
+	if err != nil {
+		return err
+	}
+	_, err = c.out.Write(buffer[:i])
+
+	return err
+}
+
+func (c *TnClient) Close() error {
+	var err error
+	if c.conn != nil {
+		err = c.conn.Close()
+	}
+	if c.in != nil {
+		err = c.in.Close()
+	}
+	return err
+}
